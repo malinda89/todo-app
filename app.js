046fbb081eebe31cd51270e9865f9cdf02b3449e
Malinda <malinda.iw@gmail.com>
2016-10-12 09:37:29 +0530

    Establish a MongoDB connection (hosted in mlab)

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://mongo:mongo99@ds053176.mlab.com:53176/db-todo', function(err) {
+  if (err) {
+    console.log('Cannot connect to DB, Error: ' + err);
+  } else {
+    console.log('Connected to DB successfully.');
+  }
+});
 
 app.use(express.static('public'));
 app.use('/', express.static('public/views'));
