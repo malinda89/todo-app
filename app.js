a90561f8a39b1fa978a851ec42c52051adad585b
Malinda <malinda.iw@gmail.com>
2016-10-13 05:51:05 +0530

    Implement item save/retrieve functions

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const todoTask = require('./models/task');
 
 const app = express();
+const router = express.Router();
 
+app.use(express.static('public'));
+app.use('/', express.static('public/views'));
+app.use('/', router);
+
+//Establish DB connection
 mongoose.connect('mongodb://mongo:mongo99@ds053176.mlab.com:53176/db-todo', function(err) {
   if (err) {
     console.log('Cannot connect to DB, Error: ' + err);
@@ -11,9 +18,42 @@ mongoose.connect('mongodb://mongo:mongo99@ds053176.mlab.com:53176/db-todo', func
   }
 });
 
-app.use(express.static('public'));
-app.use('/', express.static('public/views'));
-
+//Open port 8000 for app
 app.listen(8000, () => {
   console.log('Listening on 8000...');
 });
+
+//Register api routes
+router.route('/tasks')
+  
+  .post(function(req, res) {
+
+    let task = new todoTask();
+    console.log(req);
+    task.name = 'hardcoded-task';
+
+    //save task
+    task.save(function(err, task) {
+      if (err) {
+        console.log('Task creation unsuccessful! Error: ' + err);
+        res.send(err);
+      }
+
+      res.json({
+        savedItem: task,
+        message: 'Task created!'
+      });
+    });
+  })
+
+  .get(function(req, res) {
+    //get tasks
+    todoTask.find(function(err, tasks) {
+      if (err) {
+        console.log('Cannot retrieve tasks! Error: ' + err);
+        res.send(err);
+      }
+
+      res.json(tasks);
+    });
+  });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,20 @@
 $(function() {
-	//TODO
+
+  $.get('/tasks', (res) => {
+    let list = $('#todo_list');
+
+    res.forEach(function(item) {
+      list.append('<li>'+ item.name +'</li>');
+    })    
+  });
+
+  $('#create_task').on('click', function() {
+    const parameters = {
+      name: $('#task_name').val()
+    };
+
+    $.post('/tasks', parameters, (res) => {
+      debugger;
+    }, 'json');
+  });
 });
